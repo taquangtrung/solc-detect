b071891619ef80f603a972f7b9fd6f0f45c1dc1d
Ta Quang Trung <taquangtrungvn@gmail.com>
2023-02-15 22:54:58 +0800

    update package and gitignore

--- setup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-with open('requirements.txt', 'r') as f:
-    install_requires = f.read().splitlines()
-
-setuptools.setup(name='solc_detect',
-                 packages=['solc_detect'],
-                 install_requires=install_requires)
