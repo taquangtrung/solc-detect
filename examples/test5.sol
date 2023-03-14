baa0c27e52e6dc625a663c592efe4634fc8737ea
Ta Quang Trung <taquangtrungvn@gmail.com>
2023-03-14 14:43:20 +0800

    added some failed test cases

---
+++ examples/test5.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >=0.8.7 < 0.9.0;
+
+contract Contract2 {
+    function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+        require(b <= a);
+        return a - b;
+    }
+}
